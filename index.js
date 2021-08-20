de296d68df2c71c331aef8cd4a4e996f828820cc
Jake <jacob.lindsey88@gmail.com>
2021-08-20 11:07:03 -0500

    Recommit

---
+++ index.js
@@ -0,0 +1,10 @@
+const emailField = document.getElementById('email');
+const countryField = document.getElementById('country');
+const zipField = document.getElementById('zip');
+const passwordField = document.getElementById('password');
+const passwordConfirmField = document.getElementById('password-confirm');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', validate);
+
+// function validate()
